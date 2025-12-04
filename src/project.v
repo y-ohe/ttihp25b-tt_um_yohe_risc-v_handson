f793c15b2cfd37d241491e53c5ea24189a0a9d05
yutakaohe <yutaka.ohe@kimito.dev>
2025-12-04 10:23:14 +0900

    fixed project.v (refine)

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_yohe_risc-v_handson (
+module tt_um_yohe_risc_v_handson (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
